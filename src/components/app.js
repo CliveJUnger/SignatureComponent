eaefd8b41a761cc0d06c9548468b3e8ab6ae2e3e
olive247 <clive.unger@gmail.com>
2016-07-19 14:49:46 -0500

    Integrated React-Router

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import SignHere from './signhere';
+
 
 export default class App extends Component {
   render() {
     return (
-      <div><SignHere /></div>
+      <div>{this.props.children}</div>
     );
   }
 }
