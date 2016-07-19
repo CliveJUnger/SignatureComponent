48ba20b8b4d550f3afaf968ba76b02da8100f291
olive247 <clive.unger@gmail.com>
2016-07-19 14:50:28 -0500

    Signature processing logic

--- src/components/signhere.js
+++ src/components/signhere.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import SignaturePad from 'react-signature-pad';
+import { Link } from 'react-router';
 
 class SignHere extends Component {
+  submit() {
+    var signature = this.refs.mySignature;
+    var data = signature.toDataURL('image/png');
+    if(!signature.isEmpty()) {
+      window.open(data);
+      console.log(data);
+    }
+  }
+
   render() {
     return (
-        <SignaturePad clearButton = "true" />
+      <div>
+        <SignaturePad clearButton = "true" ref="mySignature" />
+        <div className="m-signature-pad--footer">
+          <button onClick={this.submit.bind(this)} className="btn btn-primary submit-button">Submit</button>
+          <Link className="btn btn-primary back-button" to="/">Back</Link>
+        </div>
+      </div>
     );
   }
 }
+
+export default SignHere;
