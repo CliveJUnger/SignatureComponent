5a527d204297bdeabd9eb0c7bb44835c0b39b5ec
olive247 <clive.unger@gmail.com>
2016-07-19 11:35:12 -0500

    Render signature field

---
+++ src/components/signhere.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SignaturePad from 'react-signature-pad';
+
+class SignHere extends Component {
+  render() {
+    return (
+        <SignaturePad clearButton = "true" />
+    );
+  }
+}
