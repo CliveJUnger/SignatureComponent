de6156a95f83c2263dcd4fae97e0784d35590a23
olive247 <clive.unger@gmail.com>
2016-07-21 13:09:25 -0500

    Updated packages and created dumby data

---
+++ src/dumby_data.js
@@ -0,0 +1,25 @@
+export const dumby_data = {
+  renew: false
+  renewal_date:""
+  last_name: "Unger",
+  first_name: "Clive",
+  middle_name:"",
+  suffix:"",
+  ss: "123-45-6789",
+  address: {
+    street: "9612 Old Farm Rd",
+    city: "Woodway",
+    zip: "76712",
+    county:"Mclennan"
+  },
+  email:"clive.unger@gmail.com",
+  alt_email:"",
+  dob: "7/10/1997",
+  dl: {
+    number: "36738928",
+    issue_state:"TX"
+  },
+  qualification: false,
+  bond_number:"12345",
+  date_of_signing:"7/21/2016"
+};
--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import SignHere from './components/signhere';
+import Confirm from './components/confirm';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="signhere" component={SignHere} />
+        <Route path="confirm" component={Confirm} />
       </Route>
     </Router>
   </Provider>
