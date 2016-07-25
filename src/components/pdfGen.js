da1f47a62f545eeece6d3dbf7f97978415c01622
Clive Unger <clive.unger@gmail.com>
2016-07-25 17:51:42 -0500

    Form works in inches now

--- src/components/pdfGen.js
+++ src/components/pdfGen.js
@@ -1,13 +1,18 @@
-import {texas_notary_app_uri} from '../../images/texas_notary_app_uri';
+//import {texas_notary_app_uri} from '../../images/texas_notary_app_uri';
+import {top_uri} from '../../images/top_uri';
+import {bot_uri} from '../../images/bot_uri';
 import jsPDF from '../../jsPDF/dist/jspdf.min.js';
 
 export default function (signatureURI) {
-  var doc = new jsPDF();
+  var doc = new jsPDF("p","in", [8.5,11]);
 
-  doc.setFontSize(40);
-  doc.addImage(texas_notary_app_uri, 'PNG', 0,0, 215.9, 279.4);
-  doc.text(35, 25, "TEST");
-  doc.addImage(signatureURI, 'PNG', 15, 40, 180, 160);
+  doc.setFontSize(12);
+  doc.addImage(top_uri, 'PNG', 0,0, 8.5, 5.5);
+  doc.addImage(bot_uri, 'PNG', 0,5.5, 8.5, 5.5);
+
+
+  doc.text(1, 1, "TEST");
+  //doc.addImage(signatureURI, 'PNG', 15, 40, 180, 160);
   //doc.output('dataurlnewwindow');
   //doc.save()
   var blob = doc.output("blob");
